8e2b2f281886d6b009951bf7557ef71b7cc76c9b
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-16 15:07:57 +0900

    chore: add dev tools

--- flake.nix
+++ flake.nix
@@ -5,11 +5,32 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    stdenv = pkgs.stdenv;
+  in{
+    packages.x86_64-linux.fecho_cli = stdenv.mkDerivation {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.x86_64-linux.fcitx5-fecho = stdenv.mkDerivation rec {
+      buildInputs = with pkgs; [
+        fcitx5
+        fcitx5-lua
+      ];
+    };
 
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        luajitPackages.luarocks
+        luajitPackages.luarocks-nix
+        luajit
+        stylua
+
+        cargo
+        rustc
+      ];
+    };
   };
 }
